ceba3e8e575cb48a43b71ef45f1666bbde749bc9
Moritz Bergemann <moritz.bergemann@gmail.com>
2022-10-25 22:55:11 +0800

    feat: Make DAS head flexible based on inputs

--- mmseg/models/decode_heads/das_basic_head.py
+++ mmseg/models/decode_heads/das_basic_head.py
@@ -128,36 +128,45 @@ class DASBasicHead(BaseDecodeHead):
 
         self.loss_disc = CrossEntropyLoss() # M-TODO figure out if it's ok for this to be hardcoded
 
-    def forward(self, inputs):
+    def forward(self, inputs, compute_seg=True, compute_disc=True):
+        """Function to compute inference. Optionally computes either segmentation or adversarial discrimination based on input features"""
         x = inputs
-        n, _, h, w = x[-1].shape
-        # for f in x:
-        #     print(f.shape)
 
-        _c = {}
-        for i in self.in_index:
-            # mmcv.print_log(f'{i}: {x[i].shape}, {self.linear_c[str(i)]}')
-            _c[i] = self.linear_c[str(i)](x[i]).permute(0, 2, 1).contiguous() # M: Apply linear layer
-            _c[i] = _c[i].reshape(n, -1, x[i].shape[2], x[i].shape[3])
-            if i != 0:
-                _c[i] = resize( # M: Upsample
-                    _c[i],
-                    size=x[0].size()[2:],
-                    mode='bilinear',
-                    align_corners=False)
+        if compute_seg:
+            n, _, h, w = x[-1].shape
+            # for f in x:
+            #     print(f.shape)
 
-        _c = self.linear_fuse(torch.cat(list(_c.values()), dim=1))
+            _c = {}
+            for i in self.in_index:
+                # mmcv.print_log(f'{i}: {x[i].shape}, {self.linear_c[str(i)]}')
+                _c[i] = self.linear_c[str(i)](x[i]).permute(0, 2, 1).contiguous() # M: Apply linear layer
+                _c[i] = _c[i].reshape(n, -1, x[i].shape[2], x[i].shape[3])
+                if i != 0:
+                    _c[i] = resize( # M: Upsample
+                        _c[i],
+                        size=x[0].size()[2:],
+                        mode='bilinear',
+                        align_corners=False)
 
-        if self.dropout is not None:
-            x = self.dropout(_c)
+            _c = self.linear_fuse(torch.cat(list(_c.values()), dim=1))
+
+            if self.dropout is not None:
+                x = self.dropout(_c)
+            else:
+                x = _c
+            x = self.linear_pred(x)
+            seg_pred = x
         else:
-            x = _c
-        x = self.linear_pred(x)
+            seg_pred = None
 
-        # Do domain prediction
-        domain_pred = self.discriminator(x[-1])
+        if compute_disc:
+            # Do domain prediction
+            domain_pred = self.discriminator(x[-1])
+        else:
+            domain_pred = None
 
-        return x, domain_pred
+        return seg_pred, domain_pred
 
     def forward_train(self,
                       inputs,
@@ -167,14 +176,25 @@ class DASBasicHead(BaseDecodeHead):
                       train_cfg,
                       seg_weight=None):
         """Forward training function updated to account for domain adversarial predictions.
+
+        Returns dictionary of decode head losses.
         """
-        seg_logits, domain_logits = self.forward(inputs)
+        compute_seg = gt_semantic_seg != None
+        compute_disc = gt_disc != None
+
+        seg_logits, domain_logits = self.forward(inputs, compute_seg=compute_seg, compute_disc=compute_disc)
         losses = self.losses(seg_logits, gt_semantic_seg, domain_logits, gt_disc, seg_weight)
         return losses
 
     @force_fp32(apply_to=('seg_logit', 'domain_logit', )) # M-TODO loss should be weighted by the end of this function. Remember that loss function has a weight attribute? So maybe do it there
     def losses(self, seg_logit, seg_label, domain_logit, domain_label, seg_weight=None):
-        """Compute segmentation loss + adversarial loss."""
-        loss = super.losses(seg_logit, seg_label, seg_weight=seg_weight)
-        loss['loss_adv'] = self.loss_disc(domain_logit, domain_label)
+        """Compute segmentation loss + adversarial loss. If either input (segmentation & domain label) is None, the respective loss is not computed."""
+        if seg_logit != None:
+            loss = super.losses(seg_logit, seg_label, seg_weight=seg_weight)
+        else:
+            loss = dict()
+        
+        if domain_logit != None:
+            loss['loss_adv'] = self.loss_disc(domain_logit, domain_label)
+        
         return loss
