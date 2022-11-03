3c646a6125d243b0e8009d798bcb0e6281b85d92
moritz <moritz.bergemann@gmail.com>
2022-11-03 22:44:04 +0800

    feat: Add multi-head wrappers for loss & discriminators

--- mmseg/models/discriminators/__init__.py
+++ mmseg/models/discriminators/__init__.py
@@ -2,8 +2,10 @@ from .gradient_reversal import GradientReversal
 from .discriminator import Discriminator
 from .conv_discriminator import ConvDiscriminator
 from .linear_discriminator import LinearDiscriminator
+from .multi_head_discriminator_wrapper import MultiHeadDiscriminatorWrapper
 
 __all__ = [
     'ConvDiscriminator',
-    'LinearDiscriminator'
+    'LinearDiscriminator',
+    'MultiHeadDiscriminatorWrapper'
 ]
--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -2,9 +2,11 @@ from .accuracy import Accuracy, accuracy
 from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy, mask_cross_entropy)
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
+from .multi_head_crossentropy_wrapper import MultiHeadCrossentropyWrapper
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
-    'weight_reduce_loss', 'weighted_loss'
+    'weight_reduce_loss', 'weighted_loss', 
+    'MultiHeadCrossentropyWrapper',
 ]
